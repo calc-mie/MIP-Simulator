5e0d79e9e768aef7a70346f01341248f775f1c4a
akh <akhs3214@gmail.com>
2018-06-24 08:22:59 +0900

    Refactor graph.js

--- graph.js
+++ graph.js
@@ -1,49 +1,51 @@
 class Graph {
     constructor() {
-        this.data = [1000,10000];
+        this.data = [];
         this.options = {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: '人数の遷移'
-            },
-            xAxis: {
-                categories: ['0','1','2','3','4','5','6','7']
-            },
-            yAxis: {
+                chart: {
+                    type: 'line'
+                },
                 title: {
-                    text: '人数 (人)'
-                }
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: true
-                    },
-                    enableMouseTracking: false
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            series: [{
-                data: this.data
-            }]
-        };
+                    text: '人数の遷移'
+                },
+                xAxis: {
+                    categories: ['0','1']
+                },
+                yAxis: {
+                    title: {
+                        text: '人数 (人)'
+                    }
+                },
+                plotOptions: {
+                    line: {
+                        dataLabels: {
+                            enabled: true
+                        },
+                        enableMouseTracking: false
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                series: [{
+                    data: this.data
+                }]
+            };
         this.graph = new Highcharts.chart('container', this.options);
     }
     append(n) {
         this.data.push(n);
+        this.replot();
     }
-    plot() {
+    replot() {
         this.graph.destroy();
         this.graph = new Highcharts.chart('container', this.options);
     }
 }
 
-graph = new Graph();
-
+var graph = new Graph();
 graph.append(10);
 graph.append(100);
-graph.plot();
+graph.append(100);
+graph.append(1000);
+graph.append(10000);
